3ab5590637246723e2ba9e7932742de0b654febd
Reda Ben Djelloun <reda.bendjelloun@gmail.com>
2022-04-11 15:07:07 +0200

    Affichage de l'image starfish

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+#include <Imagine/Images.h>
+using namespace Imagine;
+
+// Tableau donnant l'appartenance de chaque pixel à un unique superpixel
+//int* PartitionPixels  = new int[w*h];
+
+// Partition des 16 millions de couleurs en un nombre restreint de couleurs
+void PartitionCouleurs(int &r, int &g, int &b){
+}
+
 int main() {
-    cout<<"hello world"<<endl;
-    cout<<"non"<<endl;
-    cout<<"si"<<endl;
+    Image<Color> I;
+    if(! load(I, argc>1? argv[1]: srcPath("starfish.jpg"))) {
+        std::cout << "Echec de lecture d'image" << std::endl;
+        return 1;
+    }
+    int w = I.width();
+    int h = I.height();
+
+    openWindow(w, h);
+    display(I);
+    endGraphics();
     return 0;
 }
