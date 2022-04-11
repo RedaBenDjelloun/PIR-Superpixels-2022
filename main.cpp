88cee76cca9fa54bc709bf4e28d1976fe14a8a50
Reda Ben Djelloun <reda.bendjelloun@gmail.com>
2022-04-11 16:04:10 +0200

    partition.cpp partition.h

--- main.cpp
+++ main.cpp
@@ -5,11 +5,14 @@ using namespace std;
 #include <Imagine/Images.h>
 using namespace Imagine;
 
-// Tableau donnant l'appartenance de chaque pixel à un unique superpixel
-//int* PartitionPixels  = new int[w*h];
+const int K = 12;
 
 // Partition des 16 millions de couleurs en un nombre restreint de couleurs
 void PartitionCouleurs(int &r, int &g, int &b){
+}
+
+void drawSuperpixels(){
+
 }
 
 int main() {
