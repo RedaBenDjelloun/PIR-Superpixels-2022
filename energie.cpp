d3c8f46f1da72a045d3b4cb3c5cd9b8b3bec5373
Tri-Minh TRAM <tri-minh.tram@eleves.enpc.fr>
2022-05-16 16:20:48 +0200

    energie

--- energie.cpp
+++ energie.cpp
@@ -1,22 +1,38 @@
 #include "energie.h"
 
 /// **** Terme de couleurs ****
-//double H(Partition P){
-//    double H=0;
-//    for(int k=0;k<K;k++){
-//        double Hk=0.;
-//        for(int r=0;r<J;r++)
-//            for(int g=0;g<J;g++)
-//                for(int b=0;b<J;b++){
-//                    int val=P.get_c(k,r,g,b);
-//                    Hk+=val*val;
-//                }
-//        if(P.get_Zc(k)!=0)
-//            H+=Hk/(P.get_Zc(k)*P.get_Zc(k));
-//    }
-//    return H;
-//}
+double H(Partition &P){
+    double H=0;
+    for(int k=0;k<K;k++){
+        double Hk=0.;
+        for(int r=0;r<J;r++)
+            for(int g=0;g<J;g++)
+                for(int b=0;b<J;b++){
+                    int val=P.get_c(k,r,g,b);
+                    Hk+=val*val;
+                }
+        P.calcul_Zc(k);
+        if(P.get_Zc(k)!=0)
+            H+=Hk/(P.get_Zc(k)*P.get_Zc(k));
+    }
+    return H;
+}
+
 /// **** Terme de frontières ****
-//double G(Partition P){}
+double G(Partition &P){
+    double G=0;
+    for(int x=0;x<P.getw();x++)
+        for(int y=0;y<P.geth();y++){
+            double Gi=0.;
+            for(int k=0;k<K;k++){
+                int val=P.get_b(k,x,y);
+                Gi+=val*val;
+            }
+            P.calcul_Zb(x,y);
+            if(P.get_Zb(x,y)!=0)
+                G+=Gi/(P.get_Zb(x,y)*P.get_Zb(x,y));
+        }
+    return G;
+}
 /// **** Hill Climbing ****
 
