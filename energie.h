047da3562346e5047080d485d3e407bd95e5d75f
Tri-Minh TRAM <tri-minh.tram@eleves.enpc.fr>
2022-05-31 19:40:56 +0200

    rajout de fast_G et de cTB, version un peu plus rapide que compareTransfertBlock

--- energie.h
+++ energie.h
@@ -17,7 +17,12 @@ double G(Partition &P);
 
 /// **** Validation d'un transfert de bloc ****
 bool compareTransfertBlock(Partition &P, double H_ini, double G_ini, int x1, int y1, int wb, int hb, int k, int old_s[]);
-//si la nouvelle partition est plus petite: renvoie vrai
+//si la nouvelle partition est meilleure: renvoie vrai
+
+bool cTB(Partition &P, double H_ini, double G_ini, int x1, int y1, int wb, int hb, int k, int old_s[]);
+//si la nouvelle partition est meilleure: renvoie vrai
 
 //s_i : superpixel donneur, s_f : receveur; return true si meilleure partition pour energie H
 bool compare_fast_H(Partition &P, int x1, int y1, int wb, int hb, int s_i, int s_f);
+
+bool compare_fast_G(Partition &P, int x1, int y1,int n);
